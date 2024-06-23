b2a9797388217c98c32d5b3a22a99bb73190c552
michaellemke <decart83@gmail.com>
2024-06-23 18:33:10 -0400

    4o model

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import os
 import streamlit as st
 from langchain_core.messages import HumanMessage, AIMessage
-from openai import OpenAI
+import openai
 
 # Set your OpenAI API key
-os.environ['OPENAI_API_KEY'] = st.secrets["API_KEY"]
+openai.api_key = st.secrets["API_KEY"]
 
 client = OpenAI()
 
@@ -39,7 +39,7 @@ st.write("")
 topicListPrompt = "You are a social media consultant. The client has asked you for a plain text list of " + str(postCount) + "social media posts about " + str(topic) + ". Generate a list of " + str(postCount) + " post titles about " + str(topic)+ ". Titles need to be witten in the voice of " + str(voice) +" but do not mention" + str(voice) + ". Return a topic name, topic number and topic description in a JSON format."
 if st.button('Generate topics'):
     with st.spinner("thinking..."):
-        result = client.chat.completions.create(
+        result = openai.chat.completions.create(
             model="gpt-3.5-turbo",
             messages=[
                 {"role": "system", "content": "You are a political analyst doing marketing."},
@@ -57,7 +57,7 @@ st.write("")
 
 postListPrompt = "You are a social media consultant. The client has approved the topic list that is found here: " + topicList + ". Write the content for each post. Write in a narrative style with the voice of " + str(voice) + "Ensure everyone votes for " + str(voteFor) + "running for the position of " + str(position) + "."
 if st.button('Generate posts'):
-        result = client.chat.completions.create(
+        result = openai.chat.completions.create(
             model="gpt-3.5-turbo",
             messages=[
                 {"role": "system", "content": "You are a political analyst doing marketing."},
